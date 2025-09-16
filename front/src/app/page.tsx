b89892b5d245ce3ab58e67bec08541d8abe751fa
Aleksander <aprokhorenkodev@gmail.com>
2025-09-16 13:58:47 +0200

    feat: add return type in the main page

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-	return <h1>Main page</h1>
-}
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Main page</h1>
+    </div>
+  );
+};
+
+export default Home;
