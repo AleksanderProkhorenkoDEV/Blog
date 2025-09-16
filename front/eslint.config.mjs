66b2e71df5fdd3bc6dc5eabf32326767230b6daf
Aleksander <aprokhorenkodev@gmail.com>
2025-09-16 13:58:35 +0200

    feat: add rules to ci

--- front/eslint.config.mjs
+++ front/eslint.config.mjs
@@ -1,16 +1,28 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-})
+  baseDirectory: __dirname,
+});
+
+const rules = {
+  "no-console": "warn",
+  "no-unused-vars": "off",
+  "no-magic-numbers": "warn",
+  semi: ["error", "always"],
+  camelcase: "error",
+  "no-console": "warn",
+};
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-]
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules,
+  },
+];
 
-export default eslintConfig
+export default eslintConfig;
